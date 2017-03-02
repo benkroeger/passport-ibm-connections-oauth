a09872347776278f6d14fdbee9e102a3371cc4a9
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2017-03-02 14:20:24 +0100

    Squashed commit of the following:
    commit c073026db26c0d6847374569c671ea7786cc789e
    Merge: 4522b1b 317fc3d
    Author: Benjamin Kroeger <benjamin.kroeger@gmail.com>
    Date:   Thu Mar 2 14:19:06 2017 +0100
    
        Merge branch 'feature/configurable-endpoints' into develop
    
        * feature/configurable-endpoints:
          allow for configration of token endpoints
          upgrade to new module layout
    
    commit 317fc3dbbb0b0a2d4017939904ec86b7359ea255
    Author: Benjamin Kroeger <benjamin.kroeger@gmail.com>
    Date:   Thu Mar 2 14:15:51 2017 +0100
    
        allow for configration of token endpoints
    
        - authorizationURL
        - tokenURL
        - profileURL
    
    commit fd8882ba66aa7d51628f3805659bb7c4ea577d44
    Author: Benjamin Kroeger <benjamin.kroeger@gmail.com>
    Date:   Thu Mar 2 14:15:09 2017 +0100
    
        upgrade to new module layout

--- test/test.js
+++ test/test.js
@@ -1,11 +1,9 @@
-/*global describe, it */
 'use strict';
-var assert = require('assert');
-var passportIbmConnectionsCloud = require('../');
 
-describe('passport-ibm-connections-cloud node module', function () {
-  it('must have at least one test', function () {
-    passportIbmConnectionsCloud();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
-  });
+import test from 'ava';
+import gravity from '../lib/index';
+
+test('awesome:test', (t) => {
+  const message = 'everything is awesome';
+  t.is(gravity('awesome'), message, message);
 });
