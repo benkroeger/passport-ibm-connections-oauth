a5f807b0fdce3822b906b20b17e16c19df1fc6c1
Benjamin Kröger <benjamin.kroeger@gis-ag.com>
2017-11-08 19:21:12 +0100

    code formatting

--- test/test.js
+++ test/test.js
@@ -1,15 +1,21 @@
-'use strict';
+// node core modules
 
+// 3rd party modules
 import test from 'ava';
+
+// internal modules
 import { Strategy } from '../lib/index';
 
 test('calling constructor with bare minimum', (t) => {
   const myTest = () =>
-    new Strategy({
-      hostname: 'my.hostname.com',
-      callbackURL: 'https://my.host.com/callback',
-      clientID: '1234',
-      clientSecret: 'sssshhhh',
-    }, () => {});
+    new Strategy(
+      {
+        hostname: 'my.hostname.com',
+        callbackURL: 'https://my.host.com/callback',
+        clientID: '1234',
+        clientSecret: 'sssshhhh',
+      },
+      () => {}
+    );
   t.notThrows(myTest);
 });
